f52f8ca6421750596c4ecf9ee25a122ca401329f
Ryu Pradana <ryuprad@gmail.com>
2022-12-08 20:45:54 -0500

    Added event listeners for touch and mouse events to canvas for start and draw functions

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ canvas.height = 480
 let context = canvas.getContext("2d");
 context.fillStyle = "white";
 context.fillRect(0, 0, canvas.width, canvas.height)
+
+let draw_color = "black";
+let draw_width = "2";
+let is_drawing = false;
+
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("touchmove", draw, false);
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("mousemove", draw, false);
