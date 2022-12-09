fca7bc04997f5a97134e2f1e7aa71edaec53f32d
Ryu Pradana <ryuprad@gmail.com>
2022-12-08 20:53:03 -0500

    Added code to start drawing on mouse click event and prevent default behavior

--- script.js
+++ script.js
@@ -10,7 +10,17 @@ let draw_color = "black";
 let draw_width = "2";
 let is_drawing = false;
 
+
 canvas.addEventListener("touchstart", start, false);
 canvas.addEventListener("touchmove", draw, false);
 canvas.addEventListener("mousedown", start, false);
 canvas.addEventListener("mousemove", draw, false);
+
+
+function start(event) {
+    is_drawing = true;
+    context.beginPath();
+    context.moveTo(event.clientX - canvas.offsetLeft,
+                   event.clientY - canvas.offsetTop);
+    event.preventDefault();              
+}
